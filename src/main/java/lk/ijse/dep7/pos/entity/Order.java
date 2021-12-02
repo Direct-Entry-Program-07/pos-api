1542f912fb8269842e13b7c27116e23357b2b241
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-02 19:34:56 +0530

    Revamp Order entity with Bi-directional

--- src/main/java/lk/ijse/dep7/pos/entity/Order.java
+++ src/main/java/lk/ijse/dep7/pos/entity/Order.java
@@ -1,11 +1,10 @@
 package lk.ijse.dep7.pos.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Date;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -20,4 +19,15 @@ public class Order implements SuperEntity {
     @ManyToOne
     @JoinColumn(name = "customer_id", referencedColumnName = "id", nullable = false)
     private Customer customer;
+    @Setter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Set<OrderDetail> orderDetails;
+
+    public Order(String id, Date date, Customer customer, Set<OrderDetail> orderDetails) {
+        this.id = id;
+        this.date = date;
+        this.customer = customer;
+        this.orderDetails = orderDetails;
+        this.orderDetails.forEach(od -> od.getOrderDetailPK().setOrderId(id));
+    }
 }

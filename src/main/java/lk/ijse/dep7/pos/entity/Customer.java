dfcc062ad2e72632f66d4f8c21c569acd098ccad
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-02 18:50:33 +0530

    Revamp Customer entity with JPA annotations

--- src/main/java/lk/ijse/dep7/pos/entity/Customer.java
+++ src/main/java/lk/ijse/dep7/pos/entity/Customer.java
@@ -1,9 +1,16 @@
 package lk.ijse.dep7.pos.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "customer")
 @Entity
 public class Customer implements SuperEntity {
@@ -11,46 +18,4 @@ public class Customer implements SuperEntity {
     private String id;
     private String name;
     private String address;
-
-    public Customer() {
-    }
-
-    public Customer(String id, String name, String address) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }

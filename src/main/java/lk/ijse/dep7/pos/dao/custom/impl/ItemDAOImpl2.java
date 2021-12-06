4c188a1ff27ea04b7dd42a1922673605ccd3b7b3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-06 13:00:51 +0530

    Add ItemDAOImpl2

--- src/main/java/lk/ijse/dep7/pos/dao/custom/impl/ItemDAOImpl2.java
+++ src/main/java/lk/ijse/dep7/pos/dao/custom/impl/ItemDAOImpl2.java
@@ -9,59 +9,65 @@ import java.util.Optional;
 
 public class ItemDAOImpl2 implements ItemDAO {
 
-    private Session session;
+    private final Session session;
 
     public ItemDAOImpl2(Session session) {
         this.session = session;
     }
 
     @Override
-    public void save(Item entity) throws Exception {
+    public void save(Item entity) {
         session.save(entity);
     }
 
     @Override
-    public void update(Item entity) throws Exception {
+    public void update(Item entity) {
         session.update(entity);
     }
 
     @Override
-    public void deleteById(String key) throws Exception {
-        session.delete(session.get(Item.class, key));
+    public void deleteById(String key) {
+        Item item = session.find(Item.class, key);
+        session.delete(item);
     }
 
     @Override
-    public Optional<Item> findById(String key) throws Exception {
-        Item item = session.get(Item.class, key);
-        return (item == null)? Optional.empty(): Optional.of(item);
+    public Optional<Item> findById(String key) {
+        Item item = session.find(Item.class, key);
+        if (item == null){
+            return Optional.empty();
+        }else{
+            return Optional.of(item);
+        }
     }
 
     @Override
-    public List<Item> findAll() throws Exception {
-        return session.createQuery("FROM Item").list();
+    public List<Item> findAll() {
+        return session.createQuery("FROM Item i").list();
     }
 
     @Override
-    public long count() throws Exception {
+    public long count() {
         return session.createQuery("SELECT COUNT(i) FROM Item i", Long.class).uniqueResult();
     }
 
     @Override
-    public boolean existsById(String key) throws Exception {
+    public boolean existsById(String key) {
         return findById(key).isPresent();
     }
 
     @Override
-    public List<Item> findAll(int page, int size) throws Exception {
+    public List<Item> findAll(int page, int size) {
         return session.createNativeQuery("SELECT * FROM item LIMIT ?1 OFFSET ?2")
-                .setParameter(1, page)
-                .setParameter(2, size)
+                .setParameter(1, size)
+                .setParameter(2, size * (page - 1))
                 .addEntity(Item.class)
                 .list();
     }
 
     @Override
-    public String getLastItemCode() throws Exception {
-        return session.createNativeQuery("SELECT code FROM item ORDER BY code DESC LIMIT 1", String.class).uniqueResult();
+    public String getLastItemCode() {
+        session.flush();
+        return (String) session.createNativeQuery("SELECT code FROM item ORDER BY code DESC LIMIT 1").uniqueResult();
     }
 }

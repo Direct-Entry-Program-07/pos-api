f246e03d4600c323e23d9b8f8db32757dbd28598
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-06 17:45:26 +0530

    Revamp ItemServiceImpl

--- src/main/java/lk/ijse/dep7/pos/service/custom/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/dep7/pos/service/custom/impl/ItemServiceImpl.java
@@ -2,9 +2,11 @@ package lk.ijse.dep7.pos.service.custom.impl;
 
 import lk.ijse.dep7.pos.dao.DAOFactory;
 import lk.ijse.dep7.pos.dao.DAOType;
+import lk.ijse.dep7.pos.dao.HibernateUtil;
 import lk.ijse.dep7.pos.dao.custom.ItemDAO;
 import lk.ijse.dep7.pos.dto.ItemDTO;
 import lk.ijse.dep7.pos.service.custom.ItemService;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -20,57 +22,87 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public void saveItem(ItemDTO item) throws Exception {
-        if (existItem(item.getCode())) {
-            throw new RuntimeException(item.getCode() + " already exists");
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            session.beginTransaction();
+            if (existItem(item.getCode())) {
+                throw new RuntimeException(item.getCode() + " already exists");
+            }
+            itemDAO.save(fromItemDTO(item));
+            session.getTransaction().commit();
         }
-        itemDAO.save(fromItemDTO(item));
     }
 
     @Override
     public boolean existItem(String code) throws Exception {
-        return itemDAO.existsById(code);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            return itemDAO.existsById(code);
+        }
     }
 
     @Override
     public void updateItem(ItemDTO item) throws Exception {
-        if (!existItem(item.getCode())) {
-            throw new RuntimeException("There is no such item associated with the id " + item.getCode());
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            session.beginTransaction();
+            if (!existItem(item.getCode())) {
+                throw new RuntimeException("There is no such item associated with the id " + item.getCode());
+            }
+            itemDAO.update(fromItemDTO(item));
+            session.getTransaction().commit();
         }
-        itemDAO.update(fromItemDTO(item));
     }
 
     @Override
     public void deleteItem(String code) throws Exception {
-        if (!existItem(code)) {
-            throw new RuntimeException("There is no such item associated with the id " + code);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            session.beginTransaction();
+            if (!existItem(code)) {
+                throw new RuntimeException("There is no such item associated with the id " + code);
+            }
+            itemDAO.deleteById(code);
+            session.getTransaction().commit();
         }
-        itemDAO.deleteById(code);
     }
 
     @Override
     public ItemDTO findItem(String code) throws Exception {
-        return toItemDTO(itemDAO.findById(code).orElseThrow(() -> new RuntimeException("There is no such item associated with the id " + code)));
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            return toItemDTO(itemDAO.findById(code).orElseThrow(() -> new RuntimeException("There is no such item associated with the id " + code)));
+        }
     }
 
     @Override
     public List<ItemDTO> findAllItems() throws Exception {
-        return toItemDTOList(itemDAO.findAll());
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            return toItemDTOList(itemDAO.findAll());
+        }
     }
 
     @Override
     public List<ItemDTO> findAllItems(int page, int size) throws Exception {
-        return toItemDTOList(itemDAO.findAll(page, size));
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            return toItemDTOList(itemDAO.findAll(page, size));
+        }
     }
 
     @Override
     public String generateNewItemCode() throws Exception {
-        String code = itemDAO.getLastItemCode();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            itemDAO.setSession(session);
+            String code = itemDAO.getLastItemCode();
 
-        if (code != null) {
-            int newItemCode = Integer.parseInt(code.replace("I", "")) + 1;
-            return String.format("I%03d", newItemCode);
-        } else {
-            return "I001";
+            if (code != null) {
+                int newItemCode = Integer.parseInt(code.replace("I", "")) + 1;
+                return String.format("I%03d", newItemCode);
+            } else {
+                return "I001";
+            }
         }
     }
 

bfd506c6d21789fccb416372335ef5af5c7323b1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-07 12:45:58 +0530

    Migrate CustomerDAOImpl.java to JPA

--- src/main/java/lk/ijse/dep7/pos/dao/custom/impl/CustomerDAOImpl.java
+++ src/main/java/lk/ijse/dep7/pos/dao/custom/impl/CustomerDAOImpl.java
@@ -3,13 +3,12 @@ package lk.ijse.dep7.pos.dao.custom.impl;
 import lk.ijse.dep7.pos.dao.CrudDAOImpl;
 import lk.ijse.dep7.pos.dao.custom.CustomerDAO;
 import lk.ijse.dep7.pos.entity.Customer;
-import org.hibernate.Session;
 
 public class CustomerDAOImpl extends CrudDAOImpl<Customer, String> implements CustomerDAO {
 
     @Override
     public String getLastCustomerId() {
-        session.flush();
-        return (String) session.createNativeQuery("SELECT id FROM customer ORDER BY id DESC LIMIT 1").uniqueResult();
+        em.flush();
+        return (String) em.createNativeQuery("SELECT id FROM customer ORDER BY id DESC LIMIT 1").getSingleResult();
     }
 }

4906a590a45b96b9597ad14594e8f8eefc994074
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-08 11:20:36 +0530

    Add JPAConfig

--- src/main/java/lk/ijse/dep7/pos/AppConfig.java
+++ src/main/java/lk/ijse/dep7/pos/AppConfig.java
@@ -2,7 +2,9 @@ package lk.ijse.dep7.pos;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import(JPAConfig.class)
 @ComponentScan
 @Configuration
 public class AppConfig {
---
+++ src/main/java/lk/ijse/dep7/pos/JPAConfig.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep7.pos;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JPAConfig {
+}

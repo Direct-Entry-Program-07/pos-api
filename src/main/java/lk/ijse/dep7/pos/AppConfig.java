713d1af6021437c8f96725c9fcaa44a1dd4c559f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-12-08 11:52:37 +0530

    Finalize JPA

--- src/main/java/lk/ijse/dep7/pos/AppConfig.java
+++ src/main/java/lk/ijse/dep7/pos/AppConfig.java
@@ -1,11 +1,19 @@
 package lk.ijse.dep7.pos;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Import(JPAConfig.class)
 @ComponentScan
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
 }
